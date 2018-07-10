583386397a0c8d863bae2a0a5caff9050788d35b
Mike Russo <mike.russo@flatironschool.com>
2018-07-10 18:46:17 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -2,6 +2,6 @@
 
 def crazy_strings(first_string, second_string)
   first_string = first_string.reverse.upcase
-  second_string = second_string.swapcase.gsub("s", "z")
+  second_strin = second_string.swapcase.gsub("s", "z")
   return "#{first_string} #{second_string}"
 end
